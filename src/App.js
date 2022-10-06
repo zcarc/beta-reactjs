323e4dd56696b2d658abffd5bd8cda196afa6d92
Hyunsoo Lee <wrkbrs@gmail.com>
2022-10-07 02:48:03 +0900

    Writing Markup with JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Gallery from "./Gallery.js";
-import { Profile } from "./Profile.js";
-
-export default function App() {
+export default function Bio() {
   return (
-    <div>
-      <Profile />
-      <Gallery />
-    </div>
+    <>
+      <div className="intro">
+        <h1>Welcome to my website!</h1>
+      </div>
+      <p className="summary">
+        You can find my thoughts here.
+        <br />
+        <br />
+        <b>
+          And <i>pictures</i>
+        </b>{" "}
+        of scientists!
+      </p>
+    </>
   );
 }
