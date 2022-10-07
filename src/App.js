a61925815b6b35585da5e8e3c082bfce257e6900
Hyunsoo Lee <wrkbrs@gmail.com>
2022-10-07 19:52:00 +0900

    Passing Props to a Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import { getImageUrl } from "./utils";
-
-const person = {
-  name: "Gregorio Y. Zara",
-  imageId: "7vQD0fP",
-  imageSize: "s",
-  theme: {
-    backgroundColor: "black",
-    color: "pink",
-  },
-};
-
-export default function TodoList() {
+function Card({ children }) {
   return (
-    <div style={person.theme}>
-      <h1>{person.name}'s Todos</h1>
-      <img className="avatar" src={getImageUrl(person)} alt={person.name} />
-      <ul>
-        <li>Improve the videophone</li>
-        <li>Prepare aeronautics lectures</li>
-        <li>Work on the alcohol-fuelled engine</li>
-      </ul>
+    <div className="card">
+      <div className="card-content">{children}</div>
+    </div>
+  );
+}
+
+export default function Profile() {
+  return (
+    <div>
+      <Card>
+        <h1>Photo</h1>
+        <img
+          className="avatar"
+          src="https://i.imgur.com/OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={70}
+          height={70}
+        />
+      </Card>
+      <Card>
+        <h1>About</h1>
+        <p>
+          Aklilu Lemma was a distinguished Ethiopian scientist who discovered a
+          natural treatment to schistosomiasis.
+        </p>
+      </Card>
     </div>
   );
 }
