941612445d6a6b8656241a1a0381694d687f5e6b
Hyunsoo Lee <wrkbrs@gmail.com>
2022-10-08 19:53:19 +0900

    Rendering Lists

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
-function Item({ name, importance }) {
+import { Fragment } from "react";
+
+const poem = {
+  lines: [
+    "I write, erase, rewrite",
+    "Erase again, and then",
+    "A poppy blooms.",
+  ],
+};
+
+export default function Poem() {
   return (
-    <li className="item">
-      {name}
-      {/* case 1 */}
-      {importance > 0 && " "}
-      {importance > 0 && <i>Importance: {importance}</i>}
-
-      {/* case 2 */}
-      {/* {importance > 0 && <i> Importance: {importance}</i>} */}
-
-      {/* case 3 */}
-      {/* {importance > 0 && (
-        <>
-          {" "}
-          <i>Importance: {importance}</i>
-        </>
-      )} */}
-    </li>
-  );
-}
-
-export default function PackingList() {
-  return (
-    <section>
-      <h1>Sally Ride's Packing List</h1>
-      <ul>
-        <Item importance={9} name="Space suit" />
-        <Item importance={0} name="Helmet with a golden leaf" />
-        <Item importance={6} name="Photo of Tam" />
-      </ul>
-    </section>
+    <article>
+      {poem.lines.map((line, index) => (
+        <Fragment key={index}>
+          {index > 0 && <hr />}
+          <p>{line}</p>
+        </Fragment>
+      ))}
+    </article>
   );
 }
