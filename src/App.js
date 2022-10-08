c3a445b6d4384323d2652d44f786165a91eb9be5
Hyunsoo Lee <wrkbrs@gmail.com>
2022-10-08 21:56:41 +0900

    Keeping Components Pure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import { Fragment } from "react";
+import { useState, useEffect } from "react";
+import StoryTray from "./StoryTray.js";
 
-const poem = {
-  lines: [
-    "I write, erase, rewrite",
-    "Erase again, and then",
-    "A poppy blooms.",
-  ],
-};
+let initialStories = [
+  { id: 0, label: "Ankit's Story" },
+  { id: 1, label: "Taylor's Story" },
+];
 
-export default function Poem() {
+export default function App() {
+  let [stories, setStories] = useState([...initialStories]);
+  let time = useTime();
+
+  // HACK: Prevent the memory from growing forever while you read docs.
+  // We're breaking our own rules here.
+  if (stories.length > 100) {
+    stories.length = 100;
+  }
+
   return (
-    <article>
-      {poem.lines.map((line, index) => (
-        <Fragment key={index}>
-          {index > 0 && <hr />}
-          <p>{line}</p>
-        </Fragment>
-      ))}
-    </article>
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        textAlign: "center",
+      }}
+    >
+      <h2>It is {time.toLocaleTimeString()} now.</h2>
+      <StoryTray stories={stories} />
+    </div>
   );
+}
+
+function useTime() {
+  const [time, setTime] = useState(() => new Date());
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+  return time;
 }
