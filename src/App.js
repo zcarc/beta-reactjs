190d4f235f3e1cc404954ca154f582e7fc4d0c13
Hyunsoo Lee <wrkbrs@gmail.com>
2022-10-11 01:25:47 +0900

    Queueing a Series of State Updates: Implement the state queue yourself

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import { useState } from "react";
+import { getFinalState } from "./processQueue.js";
 
-export default function RequestTracker() {
-  console.log("req");
-  const [pending, setPending] = useState(0);
-  const [completed, setCompleted] = useState(0);
+function increment(n) {
+  return n + 1;
+}
+increment.toString = () => "n => n+1";
 
-  async function handleClick() {
-    setPending((p) => p + 1);
-    await delay(3000);
-    setPending((p) => p - 1);
-    setCompleted((c) => c + 1);
-  }
-
+export default function App() {
   return (
     <>
-      <h3>Pending: {pending}</h3>
-      <h3>Completed: {completed}</h3>
-      <button onClick={handleClick}>Buy</button>
+      <TestCase baseState={0} queue={[1, 1, 1]} expected={1} />
+      <hr />
+      <TestCase
+        baseState={0}
+        queue={[increment, increment, increment]}
+        expected={3}
+      />
+      <hr />
+      <TestCase baseState={0} queue={[5, increment]} expected={6} />
+      <hr />
+      <TestCase baseState={0} queue={[5, increment, 42]} expected={42} />
     </>
   );
 }
 
-function delay(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+function TestCase({ baseState, queue, expected }) {
+  const actual = getFinalState(baseState, queue);
+  return (
+    <>
+      <p>
+        Base state: <b>{baseState}</b>
+      </p>
+      <p>
+        Queue: <b>[{queue.join(", ")}]</b>
+      </p>
+      <p>
+        Expected result: <b>{expected}</b>
+      </p>
+      <p
+        style={{
+          color: actual === expected ? "green" : "red",
+        }}
+      >
+        Your result: <b>{actual}</b> (
+        {actual === expected ? "correct" : "wrong"})
+      </p>
+    </>
+  );
 }
