9e038894c92df328206f1af95aa9a6886035b0f5
Hyunsoo Lee <wrkbrs@gmail.com>
2022-10-13 03:31:07 +0900

    Updating Objects in State: Find and fix the mutation

--- src/App.js
+++ src/App.js
@@ -1,49 +1,47 @@
 import { useState } from "react";
+import Background from "./Background.js";
+import Box from "./Box.js";
 
-export default function Scoreboard() {
-  const [player, setPlayer] = useState({
-    firstName: "Ranjani",
-    lastName: "Shettar",
-    score: 10,
+const initialPosition = {
+  x: 0,
+  y: 0,
+};
+
+export default function Canvas() {
+  const [shape, setShape] = useState({
+    color: "orange",
+    position: initialPosition,
   });
 
-  function handlePlusClick() {
-    setPlayer({
-      ...player,
-      score: player.score + 1,
+  function handleMove(dx, dy) {
+    setShape({
+      ...shape,
+      position: {
+        ...shape.position,
+        x: shape.position.x + dx,
+        y: shape.position.y + dy,
+      },
     });
   }
 
-  function handleFirstNameChange(e) {
-    setPlayer({
-      ...player,
-      firstName: e.target.value,
+  function handleColorChange(e) {
+    setShape({
+      ...shape,
+      color: e.target.value,
     });
   }
-
-  function handleLastNameChange(e) {
-    setPlayer({
-      ...player,
-      lastName: e.target.value,
-    });
-  }
-
-  console.log(player);
 
   return (
     <>
-      <label>
-        Score: <b>{player.score}</b>{" "}
-        <button onClick={handlePlusClick}>+1</button>
-      </label>
-      <label>
-        First name:
-        <input value={player.firstName} onChange={handleFirstNameChange} />
-      </label>
-      <label>
-        Last name:
-        <input value={player.lastName} onChange={handleLastNameChange} />
-      </label>
+      <select value={shape.color} onChange={handleColorChange}>
+        <option value="orange">orange</option>
+        <option value="lightpink">lightpink</option>
+        <option value="aliceblue">aliceblue</option>
+      </select>
+      <Background position={initialPosition} />
+      <Box color={shape.color} position={shape.position} onMove={handleMove}>
+        Drag me!
+      </Box>
     </>
   );
 }
