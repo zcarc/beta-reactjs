476612b1264e8cb83e2d390a5d34e0e969dfb0b0
Hyunsoo Lee <wrkbrs@gmail.com>
2022-10-16 01:27:31 +0900

    Reacting to Input with State: Refactor the imperative solution without React

--- src/index.js
+++ src/index.js
@@ -1,12 +1,70 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+let firstName = "Jane";
+let lastName = "Jacobs";
+let isEditing = false;
 
-import App from "./App";
+function handleFormSubmit(e) {
+  e.preventDefault();
+  setIsEditing(!isEditing);
+}
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+function handleFirstNameChange(e) {
+  setFirstName(e.target.value);
+}
+
+function handleLastNameChange(e) {
+  setLastName(e.target.value);
+}
+
+function setFirstName(value) {
+  firstName = value;
+  updateDOM();
+}
+
+function setLastName(value) {
+  lastName = value;
+  updateDOM();
+}
+
+function setIsEditing(value) {
+  isEditing = value;
+  updateDOM();
+}
+
+function updateDOM() {
+  if (isEditing) {
+    editButton.textContent = "Save Profile";
+    show(firstNameInput);
+    show(lastNameInput);
+    hide(firstNameText);
+    hide(lastNameText);
+  } else {
+    editButton.textContent = "Edit Profile";
+    hide(firstNameInput);
+    hide(lastNameInput);
+    show(firstNameText);
+    show(lastNameText);
+  }
+
+  firstNameText.textContent = firstName;
+  lastNameText.textContent = lastName;
+  helloText.textContent = "Hello " + firstName + " " + lastName + "!";
+}
+
+function hide(el) {
+  el.style.display = "none";
+}
+
+function show(el) {
+  el.style.display = "";
+}
+
+let form = document.getElementById("form");
+let editButton = document.getElementById("editButton");
+let firstNameInput = document.getElementById("firstNameInput");
+let firstNameText = document.getElementById("firstNameText");
+let lastNameInput = document.getElementById("lastNameInput");
+let lastNameText = document.getElementById("lastNameText");
+let helloText = document.getElementById("helloText");
+form.onsubmit = handleFormSubmit;
+firstNameInput.oninput = handleFirstNameChange;
+lastNameInput.oninput = handleLastNameChange;
