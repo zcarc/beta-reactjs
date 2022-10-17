2ad205b8ba2db4a597f05fda4d4923ee2f849a88
Hyunsoo Lee <wrkbrs@gmail.com>
2022-10-17 19:29:17 +0900

    Choosing the State Structure: Fix the disappearing selection

--- src/App.js
+++ src/App.js
@@ -1,59 +1,46 @@
 import { useState } from "react";
-import AddItem from "./AddItem.js";
-import PackingList from "./PackingList.js";
+import { initialLetters } from "./data.js";
+import Letter from "./Letter.js";
 
-let nextId = 3;
-const initialItems = [
-  { id: 0, title: "Warm socks", packed: true },
-  { id: 1, title: "Travel journal", packed: false },
-  { id: 2, title: "Watercolors", packed: false },
-];
+export default function MailClient() {
+  const [letters, setLetters] = useState(initialLetters);
+  const [highlightedId, setHighlightedId] = useState(null);
 
-export default function TravelPlan() {
-  const [items, setItems] = useState(initialItems);
-
-  const total = items.length;
-  const packed = items.filter((item) => item.packed).length;
-
-  function handleAddItem(title) {
-    setItems([
-      ...items,
-      {
-        id: nextId++,
-        title: title,
-        packed: false,
-      },
-    ]);
+  function handleHover(letterId) {
+    setHighlightedId(letterId);
   }
 
-  function handleChangeItem(nextItem) {
-    setItems(
-      items.map((item) => {
-        if (item.id === nextItem.id) {
-          return nextItem;
+  function handleStar(starredId) {
+    setLetters(
+      letters.map((letter) => {
+        if (letter.id === starredId) {
+          return {
+            ...letter,
+            isStarred: !letter.isStarred,
+          };
         } else {
-          return item;
+          return letter;
         }
       })
     );
   }
 
-  function handleDeleteItem(itemId) {
-    setItems(items.filter((item) => item.id !== itemId));
-  }
-
   return (
     <>
-      <AddItem onAddItem={handleAddItem} />
-      <PackingList
-        items={items}
-        onChangeItem={handleChangeItem}
-        onDeleteItem={handleDeleteItem}
-      />
-      <hr />
-      <b>
-        {packed} out of {total} packed!
-      </b>
+      <h2>Inbox</h2>
+      <ul>
+        {letters.map((letter) => {
+          return (
+            <Letter
+              key={letter.id}
+              letter={letter}
+              isHighlighted={letter.id === highlightedId}
+              onHover={handleHover}
+              onToggleStar={handleStar}
+            />
+          );
+        })}
+      </ul>
     </>
   );
 }
