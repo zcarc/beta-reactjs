521a6942fea4552c34d451ff687e8877357835dd
Hyunsoo Lee <wrkbrs@gmail.com>
2022-10-17 09:49:37 +0900

    Choosing the State Structure: Fix a broken packing list

--- src/App.js
+++ src/App.js
@@ -1,31 +1,59 @@
-import { useState, useEffect } from "react";
-import Clock from "./Clock.js";
+import { useState } from "react";
+import AddItem from "./AddItem.js";
+import PackingList from "./PackingList.js";
 
-function useTime() {
-  const [time, setTime] = useState(() => new Date());
-  useEffect(() => {
-    const id = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-    return () => clearInterval(id);
-  }, []);
-  return time;
-}
+let nextId = 3;
+const initialItems = [
+  { id: 0, title: "Warm socks", packed: true },
+  { id: 1, title: "Travel journal", packed: false },
+  { id: 2, title: "Watercolors", packed: false },
+];
 
-export default function App() {
-  const time = useTime();
-  const [color, setColor] = useState("lightcoral");
+export default function TravelPlan() {
+  const [items, setItems] = useState(initialItems);
+
+  const total = items.length;
+  const packed = items.filter((item) => item.packed).length;
+
+  function handleAddItem(title) {
+    setItems([
+      ...items,
+      {
+        id: nextId++,
+        title: title,
+        packed: false,
+      },
+    ]);
+  }
+
+  function handleChangeItem(nextItem) {
+    setItems(
+      items.map((item) => {
+        if (item.id === nextItem.id) {
+          return nextItem;
+        } else {
+          return item;
+        }
+      })
+    );
+  }
+
+  function handleDeleteItem(itemId) {
+    setItems(items.filter((item) => item.id !== itemId));
+  }
+
   return (
-    <div>
-      <p>
-        Pick a color:{" "}
-        <select value={color} onChange={(e) => setColor(e.target.value)}>
-          <option value="lightcoral">lightcoral</option>
-          <option value="midnightblue">midnightblue</option>
-          <option value="rebeccapurple">rebeccapurple</option>
-        </select>
-      </p>
-      <Clock color={color} time={time.toLocaleTimeString()} />
-    </div>
+    <>
+      <AddItem onAddItem={handleAddItem} />
+      <PackingList
+        items={items}
+        onChangeItem={handleChangeItem}
+        onDeleteItem={handleDeleteItem}
+      />
+      <hr />
+      <b>
+        {packed} out of {total} packed!
+      </b>
+    </>
   );
 }
